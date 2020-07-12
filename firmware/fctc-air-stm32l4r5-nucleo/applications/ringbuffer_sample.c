f62508fea26f77ddffbbb02e6c9c4b94f5a2d613
luhuadong <luhuadong@163.com>
2020-07-13 01:06:17 +0800

    ringbuffer_sample ok

--- firmware/fctc-air-stm32l4r5-nucleo/applications/ringbuffer_sample.c
+++ firmware/fctc-air-stm32l4r5-nucleo/applications/ringbuffer_sample.c
@@ -1,23 +1,30 @@
 #include <rtthread.h>
+#include <rtdevice.h>
+#include <string.h>
 
-#define RING_BUFFER_LEN     64
+#define RING_BUFFER_LEN        8
 
-struct rt_ringbuffer *rb;
-
-static void producer_thread_entry(void *arg)
-{
-
-}
+static struct rt_ringbuffer *rb;
+static char  *str = "Hello, World";
 
 static void consumer_thread_entry(void *arg)
 {
+    char ch;
 
+    while (1)
+    {
+        if (1 == rt_ringbuffer_getchar(rb, &ch))
+        {
+            rt_kprintf("[Consumer] <- %c\n", ch);
+        }
+        rt_thread_mdelay(500);
+    }
 }
 
 static void ringbuffer_sample(int argc, char** argv)
 {
     rt_thread_t tid;
-    rt_uint16_t count = 0;
+    rt_uint16_t i = 0;
 
     rb = rt_ringbuffer_create(RING_BUFFER_LEN);
     if (rb == RT_NULL)
@@ -26,7 +33,7 @@ static void ringbuffer_sample(int argc, char** argv)
         return;
     }
 
-    tid = rt_thread_create("consumer", producer_thread_entry, RT_NULL,
+    tid = rt_thread_create("consumer", consumer_thread_entry, RT_NULL,
                            1024, RT_THREAD_PRIORITY_MAX/3, 20);
     if (tid == RT_NULL)
     {
@@ -35,13 +42,17 @@ static void ringbuffer_sample(int argc, char** argv)
     rt_thread_startup(tid);
 
 
-    while (count--)
+    while (str[i] != '\0')
     {
-        
+        rt_kprintf("[Producer] -> %c\n", str[i]);
+        rt_ringbuffer_putchar(rb, str[i++]);
+        rt_thread_mdelay(500);
     }
 
+    rt_thread_delete(tid);
+    rt_ringbuffer_destroy(rb);
     
 }
 #ifdef RT_USING_FINSH
-MSH_CMD_EXPORT_ALIAS(ringbuffer_sample, Start a producer and a consumer with a ringbuffer);
+MSH_CMD_EXPORT(ringbuffer_sample, Start a producer and a consumer with a ringbuffer);
 #endif
